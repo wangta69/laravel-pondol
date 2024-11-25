e170ecdd6eaa8df5ddd93a1da57889e67085198d
younghyeong ryu <wangta69@naver.com>
2024-11-25 16:48:53 +0900

    t
    t

--- src/Services/JsonKeyValueService.php
+++ src/Services/JsonKeyValueService.php
@@ -8,11 +8,11 @@ class JsonKeyValueService
   public static function get($key)
   {
     $obj = JsonKeyValue::where('key', $key)->first();
-    return $obj ? $obj->value : null;
+    return $obj ? $obj->v : null;
   }
 
   public static function set($key, $value)
   {
-    $result =  JsonKeyValue::updateOrCreate(['key' => $key],['value' => $value]);
+    $result =  JsonKeyValue::updateOrCreate(['key' => $key],['v' => $value]);
   }
 }
