9fed8282d7b982d52c5c1d07b5b0e2f08e756517
younghyeong ryu <wangta69@naver.com>
2024-11-28 15:00:16 +0900

    Update JsonKeyValue.php

--- src/Models/JsonKeyValue.php
+++ src/Models/JsonKeyValue.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class JsonKeyValue extends Model
 {
 
-  protected $fillable = ['key', 'value'];
+  protected $fillable = ['key', 'v'];
 
   const CREATED_AT = null;
   const UPDATED_AT = null;
