8e11e6408d501736700491a21add5fa9d1636b48
pondol <y.h.ryu@tipszone.co.kr>
2025-08-25 10:20:55 +0900

    t
    t

--- src/Http/Controllers/Admin/ComponentsController.php
+++ src/Http/Controllers/Admin/ComponentsController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Pondol\Common\Http\Controllers\Admin;
 
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Artisan;
@@ -13,19 +12,23 @@ use App\Http\Controllers\Controller;
 
 class ComponentsController  extends Controller {
 
-  public function __construct() {
-
-  }
+  public function __construct() {}
 
   public function index() {
-
     return view('pondol-common::admin.component.index');
   }
 
   public function activate($component) {
-
-    echo $component;
     switch($component) {
+      case 'auth':
+        exec('composer require wangta69/laravel-auth', $output, $returnCode);
+        if ($returnCode === 0) {
+          Artisan::call('pondol:install-auth');
+          return redirect()->route('common.admin.component.index');
+        } else {
+          echo "실패: " . implode("\n", $output);
+        }
+        break;
       case 'bbs':
         exec('composer require wangta69/laravel-board', $output, $returnCode);
         if ($returnCode === 0) {
@@ -34,10 +37,16 @@ class ComponentsController  extends Controller {
         } else {
           echo "실패: " . implode("\n", $output);
         }
-        
+        break;
+      case 'chart':
+        exec('composer require wangta69/laravel-chart', $output, $returnCode);
+        if ($returnCode === 0) {
+          Artisan::call('pondol:install-chart');
+          return redirect()->route('common.admin.component.index');
+        } else {
+          echo "실패: " . implode("\n", $output);
+        }
         break;
     }
-    
   }
- 
 }
--- src/resources/views/admin/component/index.blade.php
+++ src/resources/views/admin/component/index.blade.php
@@ -1,7 +1,6 @@
 <x-dynamic-component 
   :component="'pondol-common::common-admin'" 
-  :path="['환경설정']"> 
-
+  :path="['환경설정', '컴포넌트 활성']"> 
   
   <div id="content">
     <h2 class='title'>컴포넌트 활성</h2>
@@ -10,10 +9,10 @@
       <div class="card-body">
         <table class='table table-striped'>
           <colgroup>
-              <col width='50' />
-              <col width='150' />
-              <col width='*' />
-              <col width='80' />
+            <col width='50' />
+            <col width='150' />
+            <col width='*' />
+            <col width='80' />
           </colgroup>
           <thead>
             <tr>
@@ -25,29 +24,52 @@
           </thead>
           <tbody>
 
-              <tr class="data-row">
-                  <td class='text-center'>
-                      1
-                  </td>
-                  <td class='text-center'>
-                      Laravel Board
-                  </td>
-                  <td class='text-center'>
-                      <a href="https://github.com/wangta69/laravel_board" target="_new">ttps://github.com/wangta69/laravel_board</a>
-                  </td>
+            <tr class="data-row">
+              <td class='text-center'>
+                1
+              </td>
+              <td class='text-center'>
+                  Laravel Auth
+              </td>
+              <td class='text-center'>
+                <a href="https://github.com/wangta69/laravel-auth" target="_new">ttps://github.com/wangta69/laravel-auth</a>
+              </td>
 
-                  <td class='text-center'>
-                      <a class="btn btn-danger btn-sm act-delete" href="{{ route('common.admin.component.activate', ['bbs']) }}">활성화</a>
-                  </td>
-              </tr>
-
+              <td class='text-center'>
+                <a class="btn btn-danger btn-sm act-delete" href="{{ route('common.admin.component.activate', ['auth']) }}">활성화</a>
+              </td>
+            </tr>
+            <tr class="data-row">
+              <td class='text-center'>
+                2
+              </td>
+              <td class='text-center'>
+                Laravel Board
+              </td>
+              <td class='text-center'>
+                <a href="https://github.com/wangta69/laravel_board" target="_new">ttps://github.com/wangta69/laravel_board</a>
+              </td>
+              <td class='text-center'>
+                <a class="btn btn-danger btn-sm act-delete" href="{{ route('common.admin.component.activate', ['bbs']) }}">활성화</a>
+              </td>
+            </tr>
+            <tr class="data-row">
+              <td class='text-center'>
+                3
+              </td>
+              <td class='text-center'>
+                Laravel Chart
+              </td>
+              <td class='text-center'>
+                <a href="https://github.com/wangta69/laravel-chart" target="_new">ttps://github.com/wangta69/laravel-chart</a>
+              </td>
+              <td class='text-center'>
+                <a class="btn btn-danger btn-sm act-delete" href="{{ route('common.admin.component.activate', ['chart']) }}">활성화</a>
+              </td>
+            </tr>
           </tbody>
          </table>
       </div>
     </div>
-
-
   </div><!-- .container -->
-
-
 </x-dynamic-component>
